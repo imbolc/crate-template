10dd7650c8145d079a54186ab6c66db9c3288224
imbolc <me@imbolc.name>
2024-09-13 15:13:49 +0600

    Move lints to Cargo.toml

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,32 +1,4 @@
 #![doc = include_str!("../README.md")]
-#![forbid(unsafe_code)]
-#![deny(
-    clippy::all,
-    clippy::complexity,
-    clippy::expect_used,
-    clippy::indexing_slicing,
-    clippy::panic,
-    clippy::pedantic,
-    clippy::perf,
-    clippy::style,
-    clippy::suspicious,
-    clippy::todo,
-    clippy::unimplemented,
-    clippy::unwrap_used,
-    future_incompatible,
-    keyword_idents,
-    let_underscore,
-    missing_docs,
-    nonstandard_style,
-    refining_impl_trait,
-    rust_2018_compatibility,
-    rust_2018_idioms,
-    rust_2021_compatibility,
-    rust_2024_compatibility,
-    unreachable_pub,
-    unused
-)]
-#![warn(clippy::nursery)]
 
 #[cfg(test)]
 mod tests {
