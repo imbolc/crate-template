d145538b187cf96d66b155860a6d65bbca56085e
imbolc <me@imbolc.name>
2025-02-24 11:44:26 +0600

    Up lints

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,3 +1,27 @@
+#![forbid(
+    unsafe_code,
+    future_incompatible,
+    keyword_idents,
+    let_underscore,
+    missing_docs,
+    refining_impl_trait,
+    rust_2018_compatibility,
+    rust_2021_compatibility,
+    rust_2024_compatibility
+)]
+#![warn(
+    clippy::all,
+    clippy::complexity,
+    clippy::expect_used,
+    clippy::indexing_slicing,
+    clippy::panic,
+    clippy::perf,
+    clippy::style,
+    clippy::suspicious,
+    clippy::todo,
+    clippy::unimplemented,
+    clippy::unwrap_used
+)]
 #![doc = include_str!("../README.md")]
 
 #[cfg(test)]
